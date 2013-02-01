26fdba1905fd6d4a9fb7a0f5e8421bd2d10de508
efernie <ericf2023@gmail.com>
2013-02-01 09:17:59 -0500

    browserify updates

--- src/intro.js
+++ src/intro.js
@@ -11,5 +11,20 @@
  *
  * Date: @DATE
  */
-(function( window, undefined ) {
-"use strict";
+(function (root, factory) {
+  "use strict";
+  if (typeof exports === "object") {
+    // Node. Does not work with strict CommonJS, but
+    // only CommonJS-like enviroments that support module.exports,
+    // like Node.
+    module.exports = factory();
+  } else if (typeof define === "function" && define.amd) {
+    // AMD. Register as an anonymous module.
+    define([], factory);
+  } else {
+    // Browser globals
+    root.returnExports = factory();
+  }
+}(this, function () {
+  "use strict";
+return (function( window, undefined ) {
--- src/outro.js
+++ src/outro.js
@@ -1,2 +1,5 @@
 
+  return jQuery;
 })( window );
+
+}));
